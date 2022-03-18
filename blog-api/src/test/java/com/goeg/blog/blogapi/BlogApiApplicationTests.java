ed517d20caf090d1809e4acaad42217834986787
= <gael_oeg@hotmail.com>
2022-03-17 20:42:39 -0600

    creación del proyecto y configuracion de BD

---
+++ blog-api/src/test/java/com/goeg/blog/blogapi/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goeg.blog.blogapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
